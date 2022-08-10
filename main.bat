5950b61118aa99b94d877de68c0cf755664ebe36
Sianglife <88283637+Sianglife@users.noreply.github.com>
2022-08-10 23:01:26 +0800

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,12 +1,8 @@
 @echo off
-::底下ext等號後面改為副檔名(含.)
 set ext=.enb
-::底下type等號後面改為檔案種類(全英文)
 set type=NoteFile
-::底下path等號後面改為開啟程式路徑
-set path="C:\note.exe"
+set path="C:\Program Files (x86)\Shirui\Note\Main\EasiRunner.exe"
 
-::勿動
 echo FileExtension is %ext%
 echo FileType is %type%
 assoc %ext%=%type%
